e73bf3d18b7830f026835733cc4c00c67c98ccdc
Derex <Derex@outlook.com.br>
2022-02-07 22:13:21 -0300

    ...

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Request api da NASA
 
-const url = "https://cors-anywhere.herokuapp.com/corsdemo/http://api.open-notify.org/astros.json";
+const url = "https://cors-anywhere.herokuapp.com/http://api.open-notify.org/astros.json";
 const headers = {
     headers: { Origin: window.location.host }
   }
